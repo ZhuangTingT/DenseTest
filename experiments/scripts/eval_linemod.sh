fc8e9a939c5c928ae582e48e6fb84a5a1c9cf65c
ZhuangTT <35916997+ZhuangTingT@users.noreply.github.com>
2022-11-12 15:46:04 +0800

    Update eval_linemod.sh

--- experiments/scripts/eval_linemod.sh
+++ experiments/scripts/eval_linemod.sh
@@ -6,6 +6,6 @@ set -e
 export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=0
 
-python3 ./tools/eval_linemod.py --dataset_root ./mydata/linemod/Linemod_preprocessed\
-  --model trained_checkpoints/linemod/pose_model_9_0.01310166542980859.pth\
-  --refine_model trained_checkpoints/linemod/pose_refine_model_493_0.006761023565178073.pth
+python3 ./tools/eval_linemod.py --dataset_root /kaggle/input/linemoddense/linemod/Linemod_preprocessed\
+  --model /kaggle/input/densefusionpoints/trained_checkpoints/linemod/pose_model_9_0.01310166542980859.pth\
+  --refine_model /kaggle/input/densefusionpoints/trained_checkpoints/linemod/pose_refine_model_493_0.006761023565178073.pth
