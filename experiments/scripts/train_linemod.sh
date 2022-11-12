ee7d9689bca6cf785a4592b866e0730e7d5a9648
ZhuangTT <35916997+ZhuangTingT@users.noreply.github.com>
2022-11-12 15:44:40 +0800

    Update train_linemod.sh

--- experiments/scripts/train_linemod.sh
+++ experiments/scripts/train_linemod.sh
@@ -7,4 +7,4 @@ export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=0
 
 python3 ./tools/train.py --dataset linemod\
-  --dataset_root ./datasets/linemod/Linemod_preprocessed
+  --dataset_root /kaggle/inpput/linemoddense/Linemod_preprocessed
